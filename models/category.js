c49c29c4535d855b54a63a14d2863152a0a80937
ekadk <ekadharmakusuma3@gmail.com>
2023-04-11 23:25:03 +0700

    ADD: Category model and migration

---
+++ models/category.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Category.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          args: true,
+          msg: "category is already in database!",
+        },
+        validate: {
+          notNull: { msg: "category name is required!" },
+          notEmpty: { msg: "category name is required!" },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Category",
+    }
+  );
+
+  Category.beforeCreate((category, options) => {
+    category.name = category.name.toLowerCase();
+  });
+  return Category;
+};
