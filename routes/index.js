588941faebac6b010e450c3162de01f292947ff0
ekadk <ekadharmakusuma3@gmail.com>
2023-04-11 23:20:29 +0700

    ADD: authentication in router

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
+const authentication = require("../middelwares/authentication");
 const authRouter = require("./auth");
 
 router.use("/auth", authRouter);
-
+router.use(authentication);
 router.get("/test-auth", (req, res) => {
   res.status(200).json({
     message: "authentication passed!",
